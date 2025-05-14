395e501c22ba445b73ee8c2db004381781255622
colmos768 <colmos768@hotmail.com>
2025-05-14 16:33:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -399,6 +399,10 @@ def exportar_facturacion():
     output.headers["Content-type"] = "text/csv"
     return output
 
+@app.route("/servicio")
+def servicio():
+    return render_template("servicio.html")
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
