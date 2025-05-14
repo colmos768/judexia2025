c14982a639277a8acd0f6947004f59c361e26ae3
colmos768 <colmos768@hotmail.com>
2025-05-14 15:59:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -428,8 +428,11 @@ def exportar_facturacion():
     output.headers["Content-type"] = "text/csv"
     return output
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
+
 
 
 
