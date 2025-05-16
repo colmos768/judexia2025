405fdddee71a4e0461ef78c029805f922b542f33
colmos768 <colmos768@hotmail.com>
2025-05-15 22:56:48 -0400

    Delete create_tables.py

--- create_tables.py
+++
@@ -1,8 +0,0 @@
-# create_tables.py
-from database import db
-from models import Cliente, Causa, Pago
-
-with db.engine.connect() as connection:
-    print("Creando tablas...")
-    db.create_all()
-    print("Listo.")
