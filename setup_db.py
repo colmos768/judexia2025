d805f02f364bf106b2a19f1fa872d8138f6e006b
colmos768 <colmos768@hotmail.com>
2025-05-15 22:54:17 -0400

    Update setup_db.py

--- setup_db.py
+++ setup_db.py
@@ -1,7 +1,10 @@
-from app import app
-from models import db
+from app import create_app
+from database import db
+
+app = create_app()
 
 with app.app_context():
-   db.drop_all()
-db.create_all()
-    print("Base de datos creada correctamente.")
+    db.drop_all()
+    db.create_all()
+    print("✅ Base de datos creada correctamente.")
+
